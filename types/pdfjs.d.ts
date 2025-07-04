c78d80b7ba099f0f00a2fe02210c7151e567d7b8
KevinVichi <vichicelakevin@gmail.com>
2025-07-03 21:15:39 -0500

    correion de pdfjs

---
+++ types/pdfjs.d.ts
@@ -0,0 +1,19 @@
+declare module 'pdfjs-dist/build/pdf.worker.min.mjs' {
+  const workerSrc: string;
+  export default workerSrc;
+}
+
+declare module 'pdfjs-dist/build/pdf.worker.min.js' {
+  const workerSrc: string;
+  export default workerSrc;
+}
+
+declare module 'pdfjs-dist/build/pdf.worker.mjs' {
+  const workerSrc: string;
+  export default workerSrc;
+}
+
+declare module 'pdfjs-dist/build/pdf.worker.js' {
+  const workerSrc: string;
+  export default workerSrc;
+}
